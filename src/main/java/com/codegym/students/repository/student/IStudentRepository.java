7f7212ff2752f67494d938673883b601895da2db
VuKieuAnh <vukieuanh.hnue@gmail.com>
2020-06-03 15:50:31 +0700

    ORM

---
+++ src/main/java/com/codegym/students/repository/student/IStudentRepository.java
@@ -0,0 +1,7 @@
+package com.codegym.students.repository.student;
+
+import com.codegym.students.model.Student;
+import com.codegym.students.repository.IRepository;
+
+public interface IStudentRepository extends IRepository<Student> {
+}
---
+++ src/main/java/com/codegym/students/service/student/IStudentService.java
@@ -0,0 +1,7 @@
+package com.codegym.students.service.student;
+
+import com.codegym.students.model.Student;
+import com.codegym.students.service.IGenericService;
+
+public interface IStudentService extends IGenericService<Student> {
+}
